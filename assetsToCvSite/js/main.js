a55c5ab6e0d7f2fb904bef14cdc6ca71053ca264
Fortunekganyago@gmail.com <fortunekganyago@gmail.com>
2021-12-05 22:55:47 +0200

    Home/Buttons

--- assetsToCvSite/js/main.js
+++ assetsToCvSite/js/main.js
@@ -7,7 +7,8 @@ navClose =document.getElementById('nav-close')
 /* Validate if constant exists */
 if(navToggle)
 {
-    navToggle.addEventListener('click', () =>{navMenu.classList.add('show-menu')})
+    navToggle.addEventListener('click', () =>
+    {navMenu.classList.add('show-menu')})
 }
 
 
@@ -15,18 +16,20 @@ if(navToggle)
 /* Validate if constant exists */
 if(navClose)
 {
-    navClose.addEventListener('click', () =>{navMenu.classList.remove('show-menu')})
+    navClose.addEventListener('click', () =>
+    {navMenu.classList.remove('show-menu')})
 }
 
 
 /*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll('.nav_link')
 
-function linkAction()
-{
-    const navMenu =document.getElementById('nav-menu')
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
 
 
